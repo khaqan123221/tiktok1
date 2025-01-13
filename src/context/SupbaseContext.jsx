86475ccbb433ef6e6546616e22f239256828a950
khaqan123221 <118707823+khaqan123221@users.noreply.github.com>
2025-01-13 18:37:14 +0500

    Update SupbaseContext.jsx

--- src/context/SupbaseContext.jsx
+++ src/context/SupbaseContext.jsx
@@ -22,7 +22,7 @@ export const SupbaseProvider = ({ children }) => {
       } = await supabase.auth.getSession();
       setSession(session);
 
-      const response = await axios.get("https://tiktokbackend-dqctajb3efg0ance.eastus-01.azurewebsites.net/getVideos")
+      const response = await axios.get("https://tiktok-backend1-ductggd5fce3dkbu.canadacentral-01.azurewebsites.net/getVideos")
       setReels(response.data);
       console.log(reels)
 
